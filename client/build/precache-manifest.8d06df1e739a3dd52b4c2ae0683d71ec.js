1de8d2fdb884a7af9821e990c0a1d2be398cf10e
Melissa <melissa@Ambers-MacBook-Air.local>
2020-10-29 00:31:32 +0200

    eighth commit

---
+++ client/build/precache-manifest.8d06df1e739a3dd52b4c2ae0683d71ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf7237a318b1e3ebd980d5f7dd7b432",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f593f2b8fa15148d063",
+    "url": "/static/css/2.dc17f35a.chunk.css"
+  },
+  {
+    "revision": "f4a6d1b222411a30a68d",
+    "url": "/static/css/main.f94c99be.chunk.css"
+  },
+  {
+    "revision": "2f593f2b8fa15148d063",
+    "url": "/static/js/2.1103c1cf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1103c1cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4a6d1b222411a30a68d",
+    "url": "/static/js/main.1678f3a8.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
