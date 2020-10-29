4ffec93c5b888121355705d58ad2668500e8ff11
Melissa <melissa@Ambers-MacBook-Air.local>
2020-10-29 12:39:18 +0200

    CSP 4

---
+++ client/csp.js
@@ -0,0 +1,21 @@
+module.exports = {
+    dev: {
+    "default-src": ["'self'"],
+    "style-src": [
+      "'self'",
+      "https://*.google.com",
+    ]
+    },
+    prod: {
+    "script-src": ['https://connect.facebook.net/en_US/sdk.js', 'https://apis.google.com/js/api.js', 'self', 'unsafe-inline', 'unsafe-eval'],
+    "default-src": "'self'",  // can be either a string or an array.
+    "style-src": [
+      "'self'",
+      "https://*.facebook.com",
+    ],
+    "connect-src": [
+      "'self'",
+      "https://mybackend.com"
+    ]
+    }
+  }
